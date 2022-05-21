dd79becf0005ca8a1415165da753e1485263fe80
Anonymouseyy <79242981+Anonymouseyy@users.noreply.github.com>
2022-05-21 11:42:32 -0700

    added more basic stuff and changed ideas

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, flash, redirect, render_template, request, session
+from helpers import apology
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 
 # Configure application
@@ -10,3 +11,14 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
