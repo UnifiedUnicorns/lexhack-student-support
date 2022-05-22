65e54e2036214e9f265052f0a7ea90e3bd5cd0c4
Jacob <atosotopot@gmail.com>
2022-05-22 11:27:19 -0800

    almost done!!!!!!!!!!

--- calculations.py
+++ calculations.py
@@ -19,8 +19,76 @@ def pyramid_calc(height,base,slant,perimeter):
     return [s_area, volume]
 
 
+def sphere_calc(radius):
+    radius = float(radius)
+    s_area = 4*math.pi*(radius*radius)
+    volume = 4/3*math.pi*(radius*radius*radius)
+    return [s_area, volume]
+
+
+def cyl_calc(r, h):
+    r, h = float(r), float(h)
+    base = r*r*math.pi
+    base_cir = (r+r)*math.pi
+    vol = base * h
+    sur_ar = (base_cir*h)+(2*base)
+    return [sur_ar, vol]
+
+
+def prism_calc(height,area,perimeter):
+    height,area,perimeter=float(height),float(area),float(perimeter)
+    s_area=(2*area)+(perimeter*height)
+    volume=height*area
+    return [s_area,volume]
+
+
+def circle_calc(radius):
+    radius=float(radius)
+    area=math.pi*radius*radius
+    circum=2*math.pi*radius
+    return [area,circum]
+
+
+def triangle_calc(s1,s2,s3):
+    s1, s2, s3 = float(s1), float(s2), float(s3)
+    perimeter = s1+s2+s3
+    s = perimeter/2
+    area = math.sqrt(s*(s - s1)*(s - s2)*(s - s3))
+    return [area,perimeter,s]
+
+
 def generate_quadratic():
     a=str(random.randint(1,50))
     b=str(random.randint(1,50))
     c=str(random.randint(1,50))
     return [solve_quadratic(a, b, c), [a,b,c]]
+
+
+def rect_calc(b,h):
+    b,h = float(b), float(h)
+    area = b*h
+    per = (2*b)+(2*h)
+
+    return [area, per]
+
+
+def trap_calc(h, b, b1, s, s1):
+    h, b, b1, s, s1 = float(h), float(b), float(b1), float(s), float(s1)
+    area = (b+b1)*h/2
+    per = b+b1+s+s1
+
+    return [area, per]
+
+
+def rhomb_calc(s, d, d1):
+    s, d, d1 = float(s), float(d), float(d1)
+    area = (d*d1)/2
+    per = 4*s
+
+    return [area, per]
+
+
+def calc_reg_polygon(s, l):
+  a = (s/2)/(math.tan(math.radians(360/(2*s))))
+  p = l*s
+  return (p*a)/2
--- main.py
+++ main.py
@@ -58,9 +58,112 @@ def pyramid():
     return render_template("pyramid.html")
 
 
+@app.route('/solids/sphere', methods=["GET", "POST"])
+def sphere():
+    if request.method == "POST":
+        radius = request.form.get("radius")
+        sol = calculations.sphere_calc(radius)
+
+        return render_template("sphere.html", sol=sol, r=radius)
+
+    return render_template("sphere.html")
+
+
+@app.route("/solids/cylinder", methods=["GET", "POST"])
+def cylinder():
+    if request.method == "POST":
+        r = request.form.get("r")
+        h = request.form.get("h")
+
+        sol = calculations.cyl_calc(r, h)
+
+        return render_template("cylinder.html", sol=sol, pers=[float(r), float(h)])
+
+    return render_template("cylinder.html")
+
+
+@app.route('/solids/prism', methods=["GET", "POST"])
+def prism():
+    if request.method == "POST":
+        height = request.form.get("height")
+        area = request.form.get("area")
+        perimeter = request.form.get("perimeter")
+
+        sol = calculations.prism_calc(height,area,perimeter)
+
+        return render_template("prism.html", sol=sol, pers=[height,area,perimeter])
+
+    return render_template("prism.html")
+
+
+@app.route('/shapes/quadrilateral', methods=["GET", "POST"])
+def quadrilaterals():
+    if request.method == "POST":
+        qtype = request.form.get("type")
+
+        if qtype == "rect":
+            b = request.form.get("base")
+            h = request.form.get("height")
+
+            sol = calculations.rect_calc(b,h)
+
+            return render_template("quadrilaterals.html", sol=sol, pers=[b,h], type=qtype)
+        elif qtype == "trap":
+            h = request.form.get("height")
+            b1 = request.form.get("base")
+            b2 = request.form.get("base_2")
+            s1 = request.form.get("side")
+            s2 = request.form.get("side_2")
+
+            sol = calculations.trap_calc(h, b1, b2, s1, s2)
+
+            return render_template("quadrilaterals.html", sol=sol, pers=[float(h), float(b1), float(b2), float(s1), float(s2)], type=qtype)
+        elif qtype == "rhomb":
+            s = request.form.get("side")
+            d1 = request.form.get("diag")
+            d2 = request.form.get("diag_2")
+
+            sol = calculations.rhomb_calc(s, d1, d2)
+
+            return render_template("quadrilaterals.html", sol=sol, pers=[float(s), float(d1), float(d2)], type=qtype)
+
+    return render_template("quadrilaterals.html")
+
+
 @app.route('/shapes', methods=["GET"])
 def shapes():
     return render_template("shapes.html")
+
+
+@app.route('/shapes/circle', methods=["GET", "POST"])
+def circle():
+    if request.method == "POST":
+        radius = request.form.get("radius")
+        sol = calculations.circle_calc(radius)
+
+        return render_template("circle.html", sol=sol, radius=radius)
+
+    return render_template("circle.html")
+
+
+@app.route('/shapes/triangle', methods=["GET", "POST"])
+def triangle():
+    if request.method == "POST":
+        s1 = request.form.get("s1")
+        s2 = request.form.get("s2")
+        s3 = request.form.get("s3")
+        sol = calculations.triangle_calc(s1,s2,s3)
+
+        return render_template("triangle.html", sol=sol, pers=[float(s1),float(s2),float(s3)])
+
+    return render_template("triangle.html")
+
+
+@app.route("/shapes/regularpolygons", methods=["GET", "POST"])
+def regular_polygons():
+    if request.method == "POST":
+        s = request.form.get("s")
+        l = request.form.get("l")
 
 
 def errorhandler(e):
