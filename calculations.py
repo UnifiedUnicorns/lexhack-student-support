34c9d3de22ace30e26f6edba27483f97ceadb061
Jacob <atosotopot@gmail.com>
2022-05-21 21:48:35 -0800

    html thingy figures buttons solids shapes

--- calculations.py
+++ calculations.py
@@ -11,6 +11,11 @@ def solve_quadratic(a,b,c):
 
     return sol
 
+def pyramid_calc(height,base,slant,perimeter):
+    s_area=base+(perimeter*slant*0.5)
+    volume=1/3*base*height
+    return [s_area, volume]
+
 def generate_quadratic():
     a=str(random.randint(1,50))
     b=str(random.randint(1,50))
